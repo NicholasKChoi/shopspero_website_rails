b02a60f684a39a2c9fa369e3905b2bb22b8c166a
Alex Shan <alexshan@berkeley.edu>
2017-03-08 08:40:08 +0000

    add to rails cartapp

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106090142) do
+ActiveRecord::Schema.define(version: 20170308082302) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20161106090142) do
     t.text     "description"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "url"
   end
 
   create_table "ordered_items", force: :cascade do |t|
